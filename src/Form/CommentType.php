134cbc669584c46981029335fc9332c89c69083a
Kaliom Geefker <kaliom.geefker@gmail.com>
2022-03-04 16:52:56 +0100

    CSS

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -14,7 +14,7 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content', CKEditorType::class, [
+            ->add('content', TextareaType::class, [
                 'label' => "Laisser un commentaire",
             ])
         ;
